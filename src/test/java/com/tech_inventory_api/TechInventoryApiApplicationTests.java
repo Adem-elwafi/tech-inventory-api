889555d51a7e4edf990fa1b0d97b1134887c93a3
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-03-23 22:27:18 +0100

    feat: initialize project with Spring Boot 3.5.12, Java 21 and MySQL configuration

---
+++ src/test/java/com/tech_inventory_api/TechInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_inventory_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechInventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
